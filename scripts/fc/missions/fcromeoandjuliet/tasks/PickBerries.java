26bc8f3ea4015cfb559d8582286dc37c5bbae197
Final Calibur <fmorris2@oswego.edu>
2017-07-19 19:48:21 -0400

    Much cleaner code for ABC2 reactions

--- scripts/fc/missions/fcromeoandjuliet/tasks/PickBerries.java
+++ scripts/fc/missions/fcromeoandjuliet/tasks/PickBerries.java
@@ -2,18 +2,16 @@ package scripts.fc.missions.fcromeoandjuliet.tasks;
 
 import org.tribot.api.Timing;
 import org.tribot.api.interfaces.Positionable;
-import org.tribot.api.util.abc.ABCProperties;
 import org.tribot.api2007.Inventory;
 import org.tribot.api2007.Objects;
 import org.tribot.api2007.Player;
 import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSTile;
 
-import scripts.fc.api.abc.PersistantABCUtil;
+import scripts.fc.api.abc.ABC2Reaction;
 import scripts.fc.api.generic.FCConditions;
 import scripts.fc.api.interaction.impl.objects.ClickObject;
 import scripts.fc.api.travel.Travel;
-import scripts.fc.framework.data.Vars;
 import scripts.fc.framework.task.Task;
 import scripts.fc.missions.fcromeoandjuliet.data.QuestSettings;
 
@@ -27,6 +25,8 @@ public class PickBerries extends Task
 	private final int DISTANCE_THRESHOLD = 6;
 	private final int[] BUSH_IDS = {23625, 23626};
 	
+	private ABC2Reaction reaction = new ABC2Reaction(true, ESTIMATED_WAIT);
+	
 	@Override
 	public boolean execute()
 	{
@@ -39,18 +39,11 @@ public class PickBerries extends Task
 			RSObject[] bushes = Objects.findNearest(10, BUSH_IDS);
 			if(bushes.length > 0 && new ClickObject("Pick-from", bushes[0]).execute())
 			{
-				PersistantABCUtil abc2 = Vars.get().get("abc2");
-				abc2.generateTrackers(ESTIMATED_WAIT);
-				long startTime = Timing.currentTimeMillis();
+				reaction.start();
 				boolean success = Timing.waitCondition(FCConditions.inventoryChanged(STARTING_SPACE), 4000);
+				
 				if(success)
-				{
-					ABCProperties props = Vars.get().get("abc2Props");
-					
-					props.setWaitingTime(((Long)Timing.timeFromMark(startTime)).intValue());
-					props.setWaitingFixed(true);
-					abc2.generateAndPerformReaction(props);
-				}
+					reaction.react();
 				
 				return success;
 			}
